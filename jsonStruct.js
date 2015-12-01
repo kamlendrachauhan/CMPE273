29fb4d4c75f0d4b2034b3238eff96f8ac83cf7d2
snsaraswathi <mail2sn.saraswathi@gmail.com>
2015-11-30 23:29:23 -0800

    UI files commit

---
+++ jsonStruct.js
@@ -0,0 +1,25 @@
+/*var value = {
+    english: {
+        tools: "tools",
+        message: "View in Spanish",
+        aref: "http://localhost:3000/home/spanish"
+    }, spanish: {
+        tools: "herramientas",
+        message: "Ver en Inglés",
+        aref: "http://localhost:3000/home"
+    }
+}
+*/
+var resp = {
+    "value": [
+    {
+        "url": String
+    },
+    {
+        "url": String
+    },
+    {
+        "url": String
+    }
+]
+}
